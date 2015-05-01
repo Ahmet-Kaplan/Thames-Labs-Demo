a1e2f6eb177dc149740e48022aabd616e0f56f6d
Jamie Collinson <jamiecollinson@gmail.com>
2015-05-01 13:42:19 +0100

    Initial meteor commit

--- server/model/company.js
+++
@@ -1,9 +0,0 @@
-var config = require('../config.js');
-var knex = require('knex')(config.KnexMySqlDatabaseSettings );
-var bookshelf = require('bookshelf')(knex);
-
-var Company = bookshelf.Model.extend({
-    tableName: 'companies'
-});
-
-module.exports = Company;
--- server/model/user.js
+++
@@ -1,9 +0,0 @@
-var config = require('../config.js');
-var knex = require('knex')(config.KnexMySqlDatabaseSettings );
-var bookshelf = require('bookshelf')(knex);
-
-var User = bookshelf.Model.extend({
-    tableName: 'users'
-});
-
-module.exports = User;
