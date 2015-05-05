e8062aa8f71c3eceabf20e8fb74ce2e2fc2fc218
Jamie Collinson <jamiecollinson@gmail.com>
2015-05-05 14:09:03 +0100

    Tidy folder structure

---
+++ client/company.js
@@ -0,0 +1,104 @@
+Template.companies.onRendered(function() {
+  $('.modal-trigger').leanModal();
+});
+
+Template.companies.helpers({
+  companies: function () {
+    var companyList = companies.reactive(),
+    searchString = Session.get('companySearch');
+    if (Session.get('showMyCompanies')) {
+      companyList = companyList.filter(function(company) {
+        return company.AccMgrID === Meteor.user().profile.UserID;
+      });
+    }
+    if (searchString) {
+      var index = lunr(function() {
+        this.field('Company', { boost: 10 });
+        this.field('Address');
+        this.field('PostCode');
+        this.field('City');
+        this.ref('CompanyID');
+      });
+      companyList.forEach(function(company) {
+        index.add(company);
+      });
+      var searchResults = index.search(searchString);
+      searchResults = searchResults.map(function(result) {
+        return lodash.find(companyList, function(company) {
+          return company.CompanyID == result.ref;
+        });
+      });
+      companyList = searchResults;
+    }
+    return companyList;
+  },
+  showMyCompanies: function() {
+    return Session.get('showMyCompanies');
+  },
+  companySearch: function() {
+    return Session.get('companySearch');
+  },
+  companySearchLabelClass: function() {
+    return Session.get('companySearch') ? 'active' : '';
+  }
+});
+
+Template.companies.events({
+  'click a.add': function() {
+    Meteor.call('addRandomCompany', function() {
+      Materialize.toast('Test company added', 1000, 'teal');
+    });
+  },
+  'click a.clear': function() {
+    Meteor.call('clearRandomCompanies', function() {
+      Materialize.toast('Test companies cleared', 1000, 'red');
+    });
+  },
+  'change .switch input': function(event) {
+    Session.set('showMyCompanies', event.target.checked);
+  },
+  'keyup #search, change #search': function(event) {
+    Session.set('companySearch', event.target.value);
+  }
+});
+
+Template.addCompanyModal.events({
+  'submit form': function(event) {
+
+    event.preventDefault();
+
+    var companyName = event.target.company.value,
+    address = event.target.address.value;
+
+    Meteor.call('addCompany', {
+      companyName: companyName,
+      address: address
+    }, function(){
+      event.target.reset();
+      $('.modal').closeModal();
+      Materialize.toast('Company added', 1000, 'teal');
+    });
+
+  }
+});
+
+Template.companyDetail.helpers({
+  addressString: function(company) {
+    return encodeURIComponent([
+      company.Address,
+      company.City,
+      company.Country,
+      company.PostCode
+    ].join(', '));
+  }
+})
+
+Template.companyDetail.events({
+  'click .delete-company': function(event) {
+    var companyId = this.company.CompanyID;
+    Meteor.call('deleteCompany', companyId, function() {
+      Router.go('company');
+      Materialize.toast('Company deleted', 2000, 'red');
+    })
+  }
+});
