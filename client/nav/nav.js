070515c78d55e19dfdbe75defd4bd3fd8b75b497
Jamie Collinson <jamiecollinson@gmail.com>
2015-05-13 14:23:43 +0100

    Fix bug with feedback modal not displaying after route change

--- client/nav/nav.js
+++ client/nav/nav.js
@@ -1,5 +1,8 @@
-Template.nav.events({
-  'click #feedback-link': function(event) {
+// NB! did this instead of using Template.nav.events as
+// the nav is outside of Iron Router's {{> yield}} and the
+// router seemed to destroy events on route change.
+Template.nav.onRendered(function() {
+  $('#feedback-link').click(function() {
     Modal.show('feedbackModal');
-  }
-});
+  });
+})
