4b00595464731c7c6c5f1bd78fce37de5627c807
Damien Robson <damien.robson@cambridge-software.com>
2015-06-24 15:50:18 +0100

    Such fix!

--- server/startup.js
+++ server/startup.js
@@ -17,4 +17,35 @@ Meteor.startup(function() {
     }
   });
 
+  //Keep users information sync'ed
+  var users = Meteor.users.find({}).fetch();
+
+  _.forEach(users, function(u) {
+    if (u.profile) {
+
+      if (typeof u.profile.lastLogin === "undefined") {
+
+        Meteor.users.update(u._id, {
+          $set: {
+            "profile.lastLogin": new Date()
+          }
+        });
+
+      }
+
+      if (typeof u.profile.lastActivity === "undefined") {
+
+        Meteor.users.update(u._id, {
+          $set: {
+            "profile.lastActivity": {
+              page: "",
+              url: ""
+            }
+          }
+        });
+
+      }
+    }
+  });
+
 });
