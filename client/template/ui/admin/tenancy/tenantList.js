7a91c9507850061de2964d80392beb81fe7e9a66
Damien <damien.robson@cambridgesoftware.co.uk>
2015-07-01 09:47:33 +0100

    Added tenant/user counts to admin page Corrected date/time of last user login if never logged in

--- client/template/ui/admin/tenancy/tenantList.js
+++ client/template/ui/admin/tenancy/tenantList.js
@@ -1,51 +1,70 @@
-Template.user.onRendered(function() {
-  $('[data-toggle="tooltip"]').tooltip();
+Template.user.onRendered(function () {
+    $('[data-toggle="tooltip"]').tooltip();
 });
 
 Template.tenantList.helpers({
-  tenants: function() {
-    return Tenants.find({});
-  },
-  users: function() {
-    return Meteor.users.find({
-      group: this._id
-    });
-  },
+    tenants: function () {
+        return Tenants.find({});
+    },
+    tenantCount: function () {
+        return Tenants.find({}).count();
+    },
+    users: function () {
+        return Meteor.users.find({
+            group: this._id
+        });
+    },
+    userCount: function () {
+        return Meteor.users.find({}).count();
+    }
+});
+
+
+Template.tenant.helpers({
+    userCount: function () {
+        return Meteor.users.find({
+            group: this._id
+        }).count();
+    }
 });
 
 Template.tenantList.events({
-  "click #btnAddNewTenant": function(event, template) {
-    Modal.show('addTenant');
-  }
+    "click #btnAddNewTenant": function (event, template) {
+        Modal.show('addTenant');
+    }
 });
 
 Template.tenant.events({
-  "click #btnAddNewTenantUser": function(event, template) {
-    Modal.show('addTenantUser', this);
-  },
-  "click #btnDeleteTenant": function(event, template) {
-    Tenants.remove({
-      _id: this._id
-    });
-  },
-  "click #btnEditSettings": function(event, template) {
-    Modal.show('updateTenantSettings', this);
-  },
-  'click #btnDemoData': function() {
-    Meteor.call('generateDemoData', this._id);
-  }
+    "click #btnAddNewTenantUser": function (event, template) {
+        Modal.show('addTenantUser', this);
+    },
+    "click #btnDeleteTenant": function (event, template) {
+        Tenants.remove({
+            _id: this._id
+        });
+    },
+    "click #btnEditSettings": function (event, template) {
+        Modal.show('updateTenantSettings', this);
+    },
+    'click #btnDemoData': function () {
+        Meteor.call('generateDemoData', this._id);
+    }
 });
 
 Template.user.helpers({
-  friendlyLastLogin: function(){
-    return moment(this.profile.lastLogin).fromNow();
-  }
+    friendlyLastLogin: function () {
+        if (this.profile.lastLogin === null) {
+            return "Never logged in.";
+        } else {
+            return "Last logged in " + moment(this.profile.lastLogin).fromNow();
+        }
+    }
 });
 
 Template.user.events({
-  "click #btnDeleteTenantUser": function(event, template) {
-    Meteor.users.remove({
-      _id: this._id
-    });
-  }
+    "click #btnDeleteTenantUser": function (event, template) {
+        Meteor.users.remove({
+            _id: this._id
+        });
+    }
 });
