bfceaba235bcb497f4c1a0a9cfa211b9123ba9e8
Damien <damien.robson@cambridgesoftware.co.uk>
2015-07-01 13:46:24 +0100

    Re-enabled Asana link

--- server/methods.js
+++ server/methods.js
@@ -1,25 +1,25 @@
 // Super secret server only methods
 Meteor.methods({
     sendFeedback: function (doc) {
-        //    check(doc, Schemas.Feedback);
-        //    this.unblock();
-        //    var asanaApiKey = '7U5d5HNS.9gBvXvNdFeaiNoajrOvchS7',
-        //      asanaWorkspace = '20585633191816',
-        //      asanaProject = '36900399110512';
-        //
-        //    HTTP.post('https://app.asana.com/api/1.0/tasks', {
-        //      auth: asanaApiKey + ':',
-        //      data: {
-        //        data: {
-        //          name: _.trunc(doc.message),
-        //          projects: asanaProject,
-        //          workspace: asanaWorkspace,
-        //          notes: doc.message + '\n\nurl: ' + doc.url + '\nname: ' + doc.name
-        //        }
-        //      }
-        //    }, function(error, result) {
-        //      if (error) console.log(error);
-        //    });
+        check(doc, Schemas.Feedback);
+        this.unblock();
+        var asanaApiKey = '7U5d5HNS.9gBvXvNdFeaiNoajrOvchS7',
+            asanaWorkspace = '20585633191816',
+            asanaProject = '36900399110512';
+
+        HTTP.post('https://app.asana.com/api/1.0/tasks', {
+            auth: asanaApiKey + ':',
+            data: {
+                data: {
+                    name: _.trunc(doc.message),
+                    projects: asanaProject,
+                    workspace: asanaWorkspace,
+                    notes: doc.message + '\n\nurl: ' + doc.url + '\nname: ' + doc.name
+                }
+            }
+        }, function (error, result) {
+            if (error) console.log(error);
+        });
     },
 
     switchTenancy: function (user, target) {
