322508b21f3adf6bb44ce8c664fea7a4d45c2635
Jamie Collinson <jamiecollinson@gmail.com>
2015-07-24 09:09:01 +0100

    Fix eslint violation

--- client/template/task/taskHelperTemplates.js
+++ client/template/task/taskHelperTemplates.js
@@ -66,7 +66,7 @@ Template.taskDisplay.helpers({
   tasks: function() {
     if (isDashboard()) {
       return Tasks.find(
-        { assigneeId:Meteor.userId(), completed: false },
+        { assigneeId: Meteor.userId(), completed: false },
         { sort: { dueDate: 1 } }
       );
     } else {
