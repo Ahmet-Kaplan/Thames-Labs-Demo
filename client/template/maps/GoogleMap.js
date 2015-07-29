7d7e9070c5477f26022e28fb473cf6bf9300a6c0
MaxGuit <maxime.guitet@gmail.com>
2015-07-29 16:26:18 +0100

    Update map for company on creation and detail page

--- client/template/maps/GoogleMap.js
+++ client/template/maps/GoogleMap.js
@@ -27,24 +27,48 @@ Template.map.onCreated(function() {
     // google.maps.event.addListener(map.instance, 'idle', function(event) {
 
     if (loadSwitch === false) {
+      var gc = new google.maps.Geocoder();
+      var infowindow = new google.maps.InfoWindow();
+      if(companyData.placeid !== undefined) {
+        gc.geocode({
+          'placeId': companyData.placeid
+        }, function(results, status) {
+          if (status == google.maps.GeocoderStatus.OK) {
+            pin = results[0].geometry.location;
+            if (pin !== null) {
+              var marker = new google.maps.Marker( {
+                map: map.instance,
+                position: pin,
+                title: companyData.name
+              });
+              marker.setMap(map.instance);
+              infowindow.setContent(companyData.name);
+              infowindow.open(map.instance, marker);
+              var point = new google.maps.LatLng(pin.G, pin.K);
+              map.instance.panTo(point);
+              map.instance.setZoom(16);
 
-      gc = new google.maps.Geocoder();
-      gc.geocode({
-        'address': companyData.postcode
-      }, function(results, status) {
-        if (status == google.maps.GeocoderStatus.OK) {
-          pin = results[0].geometry.location;
-
-          if (pin !== null) {
-            var point = new google.maps.LatLng(pin.A, pin.F);
-
-            map.instance.panTo(point);
-            map.instance.setZoom(16);
-
-            loadSwitch = true;
+              loadSwitch = true;
+            }
           }
-        }
-      });
+        });
+      }else {
+        gc.geocode({
+          'address': companyData.postcode
+        }, function(results, status) {
+          if (status == google.maps.GeocoderStatus.OK) {
+            pin = results[0].geometry.location;
+            if (pin !== null) {
+              var point = new google.maps.LatLng(pin.G, pin.K);
+
+              map.instance.panTo(point);
+              map.instance.setZoom(16);
+
+              loadSwitch = true;
+            }
+          }
+        });
+      }
 
     }
 
