a5b35a22d11edf052b3be5deaf1f4970dddcbab4
Tomrack <thomas.rack@aol.com>
2015-08-03 13:22:21 +0100

    added product details tooltip

--- client/template/task/taskHelperTemplates.js
+++ client/template/task/taskHelperTemplates.js
@@ -1,3 +1,6 @@
+Template.contactList.onRendered(function() {
+  $('[data-toggle="tooltip"]').tooltip();
+});
 Template.insertNewTask.helpers({
   usersAsOptions: function() {
     return Meteor.users.find({}).map(function(user) {
