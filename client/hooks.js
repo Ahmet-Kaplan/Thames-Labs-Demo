c36bfe097709c7913e88fbf1d0ae4d82d7043edc
MaxGuit <maxime.guitet@gmail.com>
2015-08-13 12:30:22 +0100

    Corrections after merging from master

--- client/hooks.js
+++ client/hooks.js
@@ -83,6 +83,26 @@ AutoForm.hooks({
       toastr.success('Contact details updated.');
     }
   },
+  editCompanyForm: {
+    before: {
+      update: function(doc) {
+        var oldValues = this.currentDoc, modifications = true;
+        $.each(['address', 'address2', 'city', 'country', 'county', 'postcode'], function(i, field) {
+          modifications =  (oldValues[field] === doc.$set[field]);
+          return modifications;
+        });
+        if(!modifications) {
+          doc.$set.lat = '';
+          doc.$set.lng = '';
+        }
+        return doc;
+      }
+    },
+    onSuccess: function() {
+      Modal.hide();
+      toastr.success('Company details updated.');
+    }
+  },
   updatePurchaseOrderForm: {
     onSuccess: function() {
       Modal.hide();
