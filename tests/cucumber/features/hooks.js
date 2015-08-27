c77583723dc2a739c1828a57be56be4296d8e431
Jason Mashinchi <jason@mashinchi.uk>
2015-08-27 12:53:04 +0100

    Fixed test

--- tests/cucumber/features/hooks.js
+++ tests/cucumber/features/hooks.js
@@ -2,17 +2,8 @@ module.exports = function() {
 
   // called before each scenario
   // analogous to a router onBeforeAction
-  this.Before('@foo', function(event, callback) {
-    // this.server.call('reset', callback);
+  this.Before(function(event, callback) {
+     this.server.call('reset', callback);
     // do things
   });
-
-  this.registerHandler('BeforeScenario', function(event, callback) {
-    // var scenario = event.getPayloadItem('scenario');
-    // console.info("\n[" + (scenario.getName()) + "](" + (scenario.getUri()) + ":" + (scenario.getLine()) + ")");
-    // callback();
-
-    this.server.call('reset', callback);
-  });
-
 };
