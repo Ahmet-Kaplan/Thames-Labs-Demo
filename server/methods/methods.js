a310cc955e819cb3b346d50e0084b69a2d969e3d
Jamie Collinson <jamiecollinson@gmail.com>
2015-08-28 10:19:51 +0100

    Separate user permissions methods and add security

--- server/methods/methods.js
+++ server/methods/methods.js
@@ -1,59 +1,5 @@
-// Super secret server only methods
 Meteor.methods({
-  checkUserRole: function(userId, roleName) {
-    var user = Meteor.users.findOne(userId);
-    if (user) {
-      if (Roles.userIsInRole(userId, "Administrator")) {
-        return true;
-      }
 
-      if (Roles.userIsInRole(userId, roleName)) {
-        return true;
-      } else {
-        return false;
-      }
-    }
-  },
-  getMaxPermission: function(userId, permissionName) {
-    var user = Meteor.users.findOne(userId);
-    if (user) {
-      var returnedValue = 'Restricted';
-
-      if (Roles.userIsInRole(userId, "Administrator")) {
-        return 'CanDelete' + permissionName;
-      }
-
-      if (Roles.userIsInRole(userId, 'CanRead' + permissionName)) {
-        returnedValue = 'CanRead' + permissionName;
-      }
-
-      if (Roles.userIsInRole(userId, 'CanCreate' + permissionName)) {
-        returnedValue = 'CanCreate' + permissionName;
-      }
-
-      if (Roles.userIsInRole(userId, 'CanEdit' + permissionName)) {
-        returnedValue = 'CanEdit' + permissionName;
-      }
-
-      if (Roles.userIsInRole(userId, 'CanDelete' + permissionName)) {
-        returnedValue = 'CanDelete' + permissionName;
-      }
-
-      return returnedValue;
-    }
-  },
-  setUserRole: function(userId, roleName, value) {
-    var user = Meteor.users.findOne(userId);
-    if (user) {
-      if (value === true && !Roles.userIsInRole(userId, roleName)) {
-        Roles.addUsersToRoles(userId, roleName);
-      }
-
-      if (value === false && Roles.userIsInRole(userId, roleName)) {
-        Roles.removeUsersFromRoles(userId, roleName);
-      }
-    }
-  },
   clearAuditLog: function() {
     AuditLog.remove({});
   },
