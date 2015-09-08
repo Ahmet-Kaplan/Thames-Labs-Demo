05d2e85f53e6480b71ab67112b2a8e2559040a47
Jason Mashinchi <jason@mashinchi.uk>
2015-09-08 10:08:56 +0100

    Tag badges allow for any entity

--- client/template/tags/tag-badges.js
+++ client/template/tags/tag-badges.js
@@ -1,30 +1,7 @@
-Template.tagBadge.helpers({
-  getRoutedType: function() {
-    var routeName = FlowRouter.getRouteName();
-    switch (routeName) {
-      case "companies":
-        return "companies";
-        break;
-      case "contacts":
-        return "contacts";
-        break;
-      case "projects":
-        return "projects";
-        break;
-      case "opportunities":
-        entityType = "opportunities";
-        break;
-      default:
-        throw new Meteor.Error("unspecified-tag-badge-route-type", "Could not determine route type for tag badges");
-    }
-  }
-});
-
 Template.tagBadge.events({
   'click a': function() {
     // turn this into a string
     var tagName = '' + this;
-
     var routeName = FlowRouter.getRouteName();
     switch (routeName) {
       case "companies":
@@ -40,16 +17,16 @@ Template.tagBadge.events({
         Session.set('opportunitiesListSearchQuery', tagName);
         break;
       default:
-        throw new Meteor.Error("unspecified-tag-badge-route-type", "Could not determine route type for tag badges");
+        break;
     }
   }
 });
 
 Template.tagBadges.helpers({
   sortedTags: function() {
-    if (this.tags) {
-      this.tags.sort();
+    if (this.doc.tags) {
+      this.doc.tags.sort();
     }
-    return this.tags;
+    return this.doc.tags;
   }
 });
