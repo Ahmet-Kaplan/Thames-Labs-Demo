4a2b682c46923f264b60f752062cb09df51f0dae
Jamie Collinson <jamiecollinson@gmail.com>
2015-09-10 09:35:59 +0100

    Move auditData out of global subscriptions

--- common/routes.js
+++ common/routes.js
@@ -72,7 +72,6 @@ router.triggers.exit(tidyUpModals);
 router.subscriptions = function() {
   this.register('userPresence', Meteor.subscribe('userPresence'));
   this.register('allNotifications', Meteor.subscribe('allNotifications'));
-  this.register('auditData', Meteor.subscribe('auditData'));
   this.register('currentTenantUserData', Meteor.subscribe('currentTenantUserData', group));
   this.register('activeTenantData', Meteor.subscribe('activeTenantData', group));
 };
@@ -132,6 +131,7 @@ router.route('/audit', {
   subscriptions: function() {
     this.register('allTenants', subs.subscribe('allTenants'));
     this.register('allUserData', subs.subscribe('allUserData'));
+    this.register('auditData', subs.subscribe('auditData'));
   },
   action: function() {
     layout.render('appLayout', {
