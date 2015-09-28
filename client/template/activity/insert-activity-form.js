b02b7b810e9b294e63b80199417db7fa5dea53fa
Max <maxime.guitet@cambridgesoftware.co.uk>
2015-09-28 18:30:50 +0100

    Removed useless occurence of function

--- client/template/activity/insert-activity-form.js
+++ client/template/activity/insert-activity-form.js
@@ -2,14 +2,6 @@ Template.insertActivityModal.helpers({
   currentDateTime: function() {
   return moment();
   },
-  contactsAsOptions: function() {
-    return this.company.contacts().map(function(contact) {
-      return {
-        'label': contact.name(),
-        'value': contact._id
-      };
-    });
-  },
   currentUser: function() {
     return Meteor.userId();
   },
