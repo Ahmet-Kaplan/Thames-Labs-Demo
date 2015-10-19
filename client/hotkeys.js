75a7f1b7ce09d10025413bbd5918ae877fc84c61
Maxime Guitet <maxime.guitet@cambridgesoftware.co.uk>
2015-10-19 18:41:35 +0100

    Set all modal headers to h4, added hotkeys for main sections

--- client/hotkeys.js
+++ client/hotkeys.js
@@ -12,4 +12,89 @@ globalHotkeys.add({
       Modal.hide('globalSearch');
     }
   }
-})
+});
+
+globalHotkeys.add({
+  combo: "ctrl+alt+c",
+  callback: function() {
+    if(!$('.modal-header h4').text()) {
+      Modal.show('insertNewCompanyModal');
+    } else if($('.modal-header h4:contains("Add new company")').length) {
+      Modal.hide();
+    }
+  }
+});
+
+globalHotkeys.add({
+  combo: "ctrl+alt+x",
+  callback: function() {
+    if(!$('.modal-header h4').text()) {
+      Modal.show('insertContactModal');
+    } else if($('.modal-header h4:contains("Add new contact")').length) {
+      Modal.hide();
+    }
+  }
+});
+
+globalHotkeys.add({
+  combo: "ctrl+alt+j",
+  callback: function() {
+    if(!$('.modal-header h4').text()) {
+      Modal.show('newProjectForm');
+    } else if($('.modal-header h4:contains("Add new project")').length) {
+      Modal.hide();
+    }
+  }
+});
+
+globalHotkeys.add({
+  combo: "ctrl+alt+p ctrl+alt+o",
+  callback: function() {
+    if(!$('.modal-header h4').text()) {
+      Modal.show('newPurchaseOrderForm');
+    } else if($('.modal-header h4:contains("Add new purchase order")').length) {
+      Modal.hide();
+    }
+  }
+});
+
+globalHotkeys.add({
+  combo: "ctrl+alt+p ctrl+alt+r",
+  callback: function() {
+    if(!$('.modal-header h4').text()) {
+      Modal.show('insertProductModal');
+    } else if($('.modal-header h4:contains("Add new product")').length) {
+      Modal.hide();
+    }
+  }
+});
+
+globalHotkeys.add({
+  combo: "ctrl+alt+o",
+  callback: function() {
+    if(!$('.modal-header h4').text()) {
+      Modal.show('insertOpportunityModal');
+    } else if($('.modal-header h4:contains("Add new opportunity")').length) {
+      Modal.hide();
+    }
+  }
+});
+
+globalHotkeys.add({
+  combo: "ctrl+alt+w",
+  callback: function() {
+    if($('.modal-header h4').text()) {
+      Modal.hide();
+    }
+  }
+});
+
+globalHotkeys.add({
+  combo: "r e a l t i m e",
+  callback: function() {
+    bootbox.alert({
+      title: "Congratulations!!!",
+      message: "You have found the secret pop up. This is of no use at all but well done!"
+    })
+  }
+});
