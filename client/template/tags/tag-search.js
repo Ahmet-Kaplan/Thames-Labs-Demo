974800a618287b2d90d7f696d191181c9f46ad6c
Jamie Collinson <jamiecollinson@gmail.com>
2015-11-01 16:00:39 +0000

    Update selectize

--- client/template/tags/tag-search.js
+++ client/template/tags/tag-search.js
@@ -1,11 +1,10 @@
 Template.tagSearch.onRendered(function() {
 
   this.collectionName = this.data.collection;
-  this.tags = new ReactiveVar([]);
   this.index = Collections[this.collectionName].index;
 
   // Function to update tags from easysearch
-  this.updateTags = (query) => {
+  this.updateOptions = (query) => {
     Meteor.call(
       'searchByCollection',
       'tags',
@@ -18,20 +17,13 @@ Template.tagSearch.onRendered(function() {
       },
       (err, res) => {
         if (err) return;
-        this.tags.set(res);
+        this.selectize.addOption(res);
+        this.selectize.refreshOptions(false);
       });
   }
-  // Get initial tag suggestions
-  this.updateTags('');
 
-  this.addTagToProps = (tag) => {
-    var searchOptions = this.index.getComponentDict().get('searchOptions');
-    var tags = searchOptions.props && searchOptions.props.tags ? searchOptions.props.tags : '';
-    tags = tags.split(',');
-    tags.push(tag);
-    tags = tags.join(',');
-    this.index.getComponentMethods().addProps('tags', tags);
-  }
+  // Get initial tag suggestions
+  this.updateOptions('');
 
   // Initialise selectize
   this.$('.tag-input').selectize({
@@ -40,38 +32,25 @@ Template.tagSearch.onRendered(function() {
     labelField: 'name',
     searchField: ['name'],
     load: (query) => {
-      this.updateTags(query);
-    },
-    onItemAdd: (value, $item) => {
-      this.addTagToProps(value);
-    },
-    onItemRemove: (value) => {
-      this.index.getComponentMethods().removeProps('tags');
+      this.updateOptions(query);
     },
     onChange: (value) => {
-      console.log(value);
+      console.log('selectize value set to:', value);
+      if (!value) return this.index.getComponentMethods().removeProps('tags');
+      this.index.getComponentMethods().addProps('tags', value.join(','));
     }
   });
 
   // Store handle to selectize
   this.selectize = this.$('.tag-input')[0].selectize;
 
-  // Update selectize options whenever this.tags is updated
-  this.autorun( () => {
-    var newTags = this.tags.get()
-    this.selectize.addOption(newTags);
-    this.selectize.refreshOptions(false);
-  });
-
   // Update selectize items when index props are updated
   this.autorun( () => {
     var searchOptions = this.index.getComponentDict().get('searchOptions');
-    //this.selectize.clear();
+    console.log("new searchOptions:", searchOptions);
+    this.selectize.clear(true);
     if (searchOptions.props && searchOptions.props.tags) {
-      _.each(searchOptions.props.tags.split(','), (tag) => {
-        console.log(tag);
-        //this.selectize.addItem(tag);
-      });
+      this.selectize.setValue(searchOptions.props.tags.split(','), true);
     }
   });
 });
