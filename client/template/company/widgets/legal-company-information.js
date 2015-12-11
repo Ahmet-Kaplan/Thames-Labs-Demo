3859f645ef57b3b6fd9dbefb7119541d4abdf258
Jamie Collinson <jamiecollinson@gmail.com>
2015-12-11 16:34:01 +0000

    Companies house listing and metadata added

--- client/template/company/widgets/legal-company-information.js
+++ client/template/company/widgets/legal-company-information.js
@@ -1,18 +1,75 @@
 // Expects arguments:
 // - company - the company object
 
+// see https://github.com/companieshouse/api-enumerations/blob/master/constants.yml
+var companiesHouseEnumerations = {
+  "company_type": {
+    "private-unlimited": "private unlimited company",
+    "ltd": "Private Limited Company",
+    "plc": "Public Limited Company",
+    "old-public-company": "Old Public Company",
+    "private-limited-guarant-nsc-limited-exemption": "Private limited by guarantee without share capital, use of 'Limited' exemption",
+    "limited-partnership": "Limited Partnership",
+    "private-limited-guarant-nsc": "Private limited by guarantee without share capital",
+    "converted-or-closed": "Converted / Closed",
+    "private-unlimited-nsc": "Private unlimited company without share capital",
+    "private-limited-shares-section-30-exemption": "Private limited company, use of 'Limited' exemption",
+    "assurance-company": "Assurance Company",
+    "oversea-company": "overseas company",
+    "eeig": "European Economic Interest Grouping (EEIG)",
+    "icvc-securities": "Investment Company with Variable Capital",
+    "icvc-warrant": "Investment Company with Variable Capital",
+    "icvc-umbrella": "Investment Company with Variable Capital",
+    "industrial-and-provident-society": "Industrial and Provident Society",
+    "northern-ireland": "Northern Ireland Company",
+    "northern-ireland-other": "Credit Union (Northern Ireland)",
+    "llp": "Limited Liability Partnership",
+    "royal-charter": "Royal Charter Company",
+    "investment-company-with-variable-capital": "investment company with variable capital",
+    "unregistered-company": "unregistered company",
+    "other": "Other Company Type",
+    "european-public-limited-liability-company-se": "European Public Limited-Liability Company (SE)"
+  },
+  "company_status": {
+    "active": "Active",
+    "dissolved": "Dissolved",
+    "liquidation": "Liquidation",
+    "receivership": "Receivership",
+    "converted-closed": "Converted / Closed",
+    "voluntary-arrangement": "Voluntary Arrangement",
+    "insolvency-proceedings": "Insolvency Proceedings",
+    "administration": "In Administration"
+  }
+};
+
 Template.legalCompanyInformation.onCreated(function() {
   this.companiesHouseSearchResults = new ReactiveVar([]);
+  this.companiesHouseSearchResultsCount = new ReactiveVar(null);
+
+  // Update companies house search if details change
   this.autorun( () => {
     var company = Template.currentData().company;
     if (company.companiesHouseId || company.country !== 'United Kingdom') {
-      this.companiesHouseSearchResults.set([]);
       return;
     }
-    console.log('fetching companies house data');
-    Meteor.call('companiesHouse.search', company.name, (err, res) => {
+    Meteor.call('companiesHouse.search.companies', company.name, (err, res) => {
       if (err) throw new Meteor.Error(err);
       this.companiesHouseSearchResults.set(res.data.items);
+      this.companiesHouseSearchResultsCount.set(res.data.total_results);
+    });
+  });
+
+  // Refresh company data from companies house
+  // Just watch companiesHouseId to prevent unwanted API calls
+  this.autorun( () => {
+    var companiesHouseId = Template.currentData().company.companiesHouseId;
+    if (!companiesHouseId) return;
+    Meteor.call('companiesHouse.company', companiesHouseId, (err, res) => {
+      if (err) throw new Meteor.Error(err);
+      Companies.update(
+        this.data.company._id,
+        { $set: { 'metadata.companiesHouse': res.data }}
+      );
     });
   });
 });
@@ -29,23 +86,45 @@ Template.legalCompanyInformation.helpers({
       result.companyId = this.company._id;
       return result;
     });
+  },
+  companiesHouseSearchResultsCount: function() {
+    return Template.instance().companiesHouseSearchResultsCount.get();
+  },
+  companiesHouseSearchResultsNotShown: function() {
+    var results = Template.instance().companiesHouseSearchResults.get(),
+        totalCount = Template.instance().companiesHouseSearchResultsCount.get();
+    return totalCount > results.length ? totalCount - results.length : 0;
+  },
+  pluralResults: function() {
+    return Template.instance().companiesHouseSearchResults.get().length != 1;
+  },
+  companiesHouseEnumerate: function(section, key) {
+    return companiesHouseEnumerations[section][key];
+  },
+  statusWarning: function() {
+    // n.b. - we're calling this in company.metadata.companiesHouse context!
+    if (this.company_status !== 'active') return true;
+    return false;
   }
 });
 
 Template.legalCompanyInformation.events({
-  'click a.unlink-record': function() {
+  'click a.unlink-record': function(e) {
+    e.preventDefault();
     Companies.update(this.company._id, {
       $unset: { companiesHouseId: 1 }
     });
+    toastr.success('Record unlinked');
   }
 });
 
 Template.legalCompanyInformationSearchResult.events({
   'click a': function(e) {
     e.preventDefault();
-    console.log(this);
     Companies.update(this.companyId, {
-      $set: { companiesHouseId: this.company_number}
+      $set: {
+        companiesHouseId: this.company_number
+      }
     });
     toastr.success('Record linked to Companies House');
   }
--- server/methods/companieshouse-api.js
+++ server/methods/companieshouse-api.js
@@ -1,6 +1,6 @@
 Meteor.methods({
 
-  'companiesHouse.search': function(companyName) {
+  'companiesHouse.search.companies': function(companyName) {
     var companiesHouseApiKey = process.env.COMPANIESHOUSE_API_KEY;
     if (typeof companiesHouseApiKey == 'undefined') {
       throw new Meteor.Error(500, 'No companies house API key set');
@@ -9,7 +9,21 @@ Meteor.methods({
     var baseurl = 'https://api.companieshouse.gov.uk/search/companies',
         auth = companiesHouseApiKey + ':';
 
-    url = encodeURI(baseurl + '?q=' + companyName);
+    url = encodeURI(baseurl + '?q=' + companyName + '&items_per_page=10');
+
+    return HTTP.get(url, {auth: auth});
+  },
+
+  'companiesHouse.company': function(companyNumber) {
+    var companiesHouseApiKey = process.env.COMPANIESHOUSE_API_KEY;
+    if (typeof companiesHouseApiKey == 'undefined') {
+      throw new Meteor.Error(500, 'No companies house API key set');
+    }
+
+    var baseurl = 'https://api.companieshouse.gov.uk/company/',
+        auth = companiesHouseApiKey + ':';
+
+    url = encodeURI(baseurl + companyNumber);
 
     return HTTP.get(url, {auth: auth});
   }
