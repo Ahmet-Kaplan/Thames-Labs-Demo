5d6f74967267caed79fdb7887cce132a64827f7d
Maxime Guitet <maxime.guitet@cambridgesoftware.co.uk>
2016-01-14 17:33:04 +0000

    Added missing new files

---
+++ server/methods/settings.js
@@ -0,0 +1,16 @@
+Meteor.methods({
+  setNewCurrency: function(currency) {
+    var allowedCurrencies = ['gbp', 'eur', 'usd'];
+    var newCurrency = (allowedCurrencies.indexOf(currency) !== -1) ? currency : 'gbp';
+
+    Tenants.update({
+      _id: Meteor.user().group
+    },{
+      $set: {
+        'settings.currency': newCurrency
+      }
+    });
+
+    return newCurrency;
+  }
+})
