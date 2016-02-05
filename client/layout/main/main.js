58d3bbdd2a6ee0c883a8ff0e8c4f479c45b3ae00
Damien <damien@cambridgesoftware.co.uk>
2016-02-05 08:44:11 +0000

    Added live support chat (provided by Tawk.to). Appears on all areas of the site, in the bottom left corner.

--- client/layout/main/main.js
+++ client/layout/main/main.js
@@ -17,33 +17,18 @@ Template.appLayout.helpers({
 
 Template.appLayout.onRendered(function() {
   $.getScript('/vendor/bowser.min.js');
-
-  this.autorun(function() {
-
-    if (Session.get('tawkLoaded') === false) {
-      var user = Meteor.user();
-
-      var Tawk_API = Tawk_API || {},
-        Tawk_LoadStart = new Date();
-
-      if (user && user.group) {
-        if (IsTenantPro(user.group)) {
-          var s1 = document.createElement("script"),
-            s0 = document.getElementsByTagName("script")[0];
-          s1.async = true;
-          s1.src = 'https://embed.tawk.to/' + Meteor.settings.public.tawkSiteId + '/default';
-          s1.charset = 'UTF-8';
-          s1.setAttribute('crossorigin', '*');
-          s0.parentNode.insertBefore(s1, s0);
-          s0.id = 'tawkScript'
-
-          Session.set('tawkLoaded', true);
-        }
-      }
-    }
-  });
 });
 
 Template.appLayout.onCreated(function() {
-  Session.set('tawkLoaded', false);
+  var Tawk_API = Tawk_API || {},
+    Tawk_LoadStart = new Date();
+
+  var s1 = document.createElement("script"),
+    s0 = document.getElementsByTagName("script")[0];
+  s1.async = true;
+  s1.src = 'https://embed.tawk.to/' + Meteor.settings.public.tawkSiteId + '/default';
+  s1.charset = 'UTF-8';
+  s1.setAttribute('crossorigin', '*');
+  s0.parentNode.insertBefore(s1, s0);
+  s0.id = 'tawkScript'
 });
