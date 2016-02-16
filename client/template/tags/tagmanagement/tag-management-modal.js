3db05d42cdec22602ef977b094ffb69f92285002
Damien <damien@cambridgesoftware.co.uk>
2016-02-16 13:01:00 +0000

    Added toggle mode to bulk management form

--- client/template/tags/tagmanagement/tag-management-modal.js
+++ client/template/tags/tagmanagement/tag-management-modal.js
@@ -1,10 +1,21 @@
 Template.tagManagementModal.onCreated(function() {
 	this.collectionName = this.data.collectionName;
+	this.addMode = new ReactiveVar(true);
 });
 
 Template.tagManagementModal.onRendered(function() {
-	var collectionName = this.collectionName;
-	var index = Collections[this.collectionName].index,
+	$('#mode-toggle').bootstrapToggle({
+		on: 'Add',
+		off: 'Remove',
+		size: 'mini',
+		onstyle: 'primary',
+		offstyle: 'danger'
+	});
+
+	var self = this;
+
+	var collectionName = self.collectionName;
+	var index = Collections[collectionName].index,
 		searchDefinition = index.getComponentDict().get('searchDefinition'),
 		searchOptions = index.getComponentDict().get('searchOptions');
 
@@ -52,12 +63,12 @@ Template.tagManagementModal.onRendered(function() {
 			Meteor.call('tag.addTagToResults', collectionName, searchDefinition, searchOptions, value);
 		},
 		onDelete: function(values) {
-			var res = confirm('Are you sure you wish to remove the selected tag?');
-			if (res) {
+			if (self.addMode.get() === true) {
+				return false;
+			} else {
 				Meteor.call('tag.removeTagFromResults', collectionName, searchDefinition, searchOptions, values);
 				return true;
 			}
-			return false;
 		}
 	});
 
@@ -72,7 +83,18 @@ Template.tagManagementModal.onRendered(function() {
 	});
 });
 
+Template.tagManagementModal.events({
+	'change #mode-toggle': function(event, template) {
+		var state = template.addMode.get();
+		template.addMode.set(!state);
+	}
+});
+
 Template.tagManagementModal.helpers({
+	showWarning: function() {
+		var state = Template.instance().addMode.get();
+		return !state;
+	},
 	tags: function() {
 		var index = Collections[this.collectionName].index,
 			searchDefinition = index.getComponentDict().get('searchDefinition'),
