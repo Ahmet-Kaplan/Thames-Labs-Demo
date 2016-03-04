8b75910269b3a0a8d9847dfea984f8a5d6df0f40
Damien <damien@cambridgesoftware.co.uk>
2016-03-04 11:29:16 +0000

    Process email by adding as activity against contacts

--- server/methods/inbox.js
+++ server/methods/inbox.js
@@ -1,5 +1,6 @@
 var multer = Meteor.npmRequire('multer');
 var upload = multer();
+var inboxAddress = 'inbox@mailgun.realtimecrm.co.uk';
 
 Picker
 	.filter(function(req, res) {
@@ -8,26 +9,7 @@ Picker
 	.route('/mailbox', function(params, req, res) {
 
 		var data = req.body;
-		var sender = data.sender;
-		var recipient = data.recipient;
-		var subject = data.subject;
-		var fullThread = data["body-plain"];
-		var mailText = data["stripped-text"];
-		var mailSignature = data["stripped-signature"];
-		var timestamp = data.timestamp;
-		var sendDate = new Date(0);
-		sendDate.setUTCSeconds(timestamp);
-
-		console.log('---------------------------');
-		console.log('MailGun Inbound Router data');
-		console.log('---------------------------');
-		console.log('Sender: ' + sender);
-		console.log('Recipient: ' + recipient);
-		console.log('Subject: ' + subject);
-		console.log('Mail Text: ' + mailText);
-		console.log('Mail Signature: ' + mailSignature);
-		console.log('Send Date: ' + sendDate);
-		console.log('---------------------------');
+		Meteor.call('mailgun.createActivityFromBodyData', data);
 
 		res.writeHead(200, {
 			'Content-Type': 'text/plain'
@@ -35,3 +17,96 @@ Picker
 		res.end('ok');
 	})
 	.middleware(upload.single());
+
+Meteor.methods({
+	'mailgun.createActivityFromBodyData': function(bodyData) {
+		var subject = bodyData.Subject;
+		var mailText = bodyData["stripped-text"];
+		var timestamp = bodyData.timestamp;
+		var sendDate = new Date(0);
+		sendDate.setUTCSeconds(timestamp);
+
+		var userEmail = bodyData.From;
+		var cs = userEmail.indexOf('<');
+		if (cs > -1) {
+			var ce = userEmail.indexOf('>');
+			var newStr = userEmail.substring(cs + 1, ce);
+			userEmail = newStr
+		}
+
+		var MeteorUser = Meteor.users.findOne({
+			'emails.address': userEmail
+		});
+
+		if (!MeteorUser) {
+			return;
+		} else {
+			var TheTenant = Tenants.findOne({
+				_id: MeteorUser.group
+			});
+			if (!TheTenant) {
+				return;
+			}
+
+			Partitioner.bindGroup(TheTenant._id, function() {
+				var addresses = bodyData.To.split(',');
+				addresses.push(bodyData.From);
+				_.each(addresses, function(addr, i) {
+					var str = addr.trim();
+					var chevronStart = str.indexOf('<');
+					if (chevronStart > -1) {
+						var chevronEnd = str.indexOf('>');
+						var newStr = str.substring(chevronStart + 1, chevronEnd);
+						addresses[i] = newStr
+					}
+				});
+
+				var uniqueAddresses = _.uniq(addresses.map(function(r) {
+					return {
+						'email': r
+					}
+				}), 'email');
+
+				_.each(uniqueAddresses, function(ua) {
+
+					if(ua.email === inboxAddress) return;
+
+					var contact = Contacts.findOne({
+						email: ua.email
+					});
+
+					if (contact) {
+						// Add an activity record against the discovered contact...
+						var activityId = Activities.insert({
+							type: 'Email',
+							notes: subject + "\n\n" + mailText,
+							createdAt: Date.now(),
+							activityTimestamp: sendDate,
+							contactId: contact._id,
+							createdBy: MeteorUser._id,
+							primaryEntityId: contact._id,
+							primaryEntityType: 'contacts',
+							primaryEntityDisplayData: contact.name()
+						});
+
+						// ...and tag it accordingly to ensure we know where it's come from
+						Activities.addTag('Automated', {
+							_id: activityId
+						});
+					}
+				})
+			});
+		}
+
+		// console.log('---------------------------');
+		// console.log('MailGun Inbound Router data');
+		// console.log('---------------------------');
+		// console.log('Sender: ' + sender);
+		// console.log('Recipient: ' + recipient);
+		// console.log('Subject: ' + subject);
+		// console.log('Mail Text: ' + mailText);
+		// console.log('Mail Signature: ' + mailSignature);
+		// console.log('Send Date: ' + sendDate);
+		// console.log('---------------------------');
+	}
+})
