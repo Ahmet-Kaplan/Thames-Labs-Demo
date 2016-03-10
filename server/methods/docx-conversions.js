914817ec4fb68613af666de2db0f3037b47808b6
Damien <damien@cambridgesoftware.co.uk>
2016-03-10 08:50:04 +0000

    Fixes #93597083920345

--- server/methods/docx-conversions.js
+++ server/methods/docx-conversions.js
@@ -6,7 +6,7 @@ Meteor.methods({
     if (count == 100 || count == 50 || count == 25 || count < 15) {
       var txt = 'Running out of doc to pdf conversions. We have ' + count + ' left';
       Email.send({
-        to: 'david.mcleary@cambridgesoftware.co.uk',
+        to: 'realtimecrm-notifications@cambridgesoftware.co.uk',
         from: 'RealTimeCRM <admin@realtimecrm.co.uk>',
         subject: 'Running out of doc to pdf conversions...',
         text: txt
