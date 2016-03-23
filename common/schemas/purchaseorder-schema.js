8995572bf90de95f5e4263039a6fce2eba29c026
Ben <ben.miles@cambridgesoftware.co.uk>
2016-03-23 16:18:04 +0000

    Remove unused part of po schema

--- common/schemas/purchaseorder-schema.js
+++ common/schemas/purchaseorder-schema.js
@@ -119,13 +119,6 @@ Schemas.PurchaseOrder = new SimpleSchema({
     autoform: {
       type: 'hidden'
     }
-  },
-  poNotificationJob: {
-    type: String,
-    optional: true,
-    autoform: {
-      type: 'hidden'
-    }
   }
 });
 Schemas.PurchaseOrder.messages({
