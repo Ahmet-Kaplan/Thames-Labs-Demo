0f99875afc2006dd9f6fb4c9de8c7dca137e15b4
Damien <damien@cambridgesoftware.co.uk>
2016-03-24 12:07:21 +0000

    Adding the start script would help. Also add a watch for chimp closing to kill everything (so that CircleCI doesn't run for eternity once the tests complete

---
+++ .scripts/start.js
@@ -0,0 +1,57 @@
+#!/usr/bin/env node
+
+const exec = require('child_process').exec,
+	path = require('path'),
+	extend = require('util')._extend;
+
+const meteorCommand = 'meteor --settings settings.json',
+	meteorProcessOptions = {
+		cwd: path.resolve(__dirname, '..'),
+		env: extend(process.env, {
+			STRIPE_PK: "pk_test_W7Cx4LDFStOIaJ2g5DufAIaE",
+			STRIPE_SK: "sk_test_CEgjj8xNKSrQMUrqC4puiHxA"
+		})
+	};
+
+const chimpCommand = 'chimp';
+var chimpSwitches = ' --ddp=http://localhost:3000 --path=tests/cucumber';
+
+if (!process.env.CI) {
+	chimpSwitches += ' --watch';
+}
+
+function startMeteor(callback) {
+	console.log('Starting meteor');
+
+	var meteorProcess = exec(meteorCommand, meteorProcessOptions);
+	meteorProcess.stdout.pipe(process.stdout);
+	meteorProcess.stderr.pipe(process.stderr);
+
+	if (callback) {
+		meteorProcess.stdout.on('data', (data) => {
+			if (data.match('App running at')) {
+				callback();
+			}
+		});
+	}
+}
+
+function startChimp() {
+	console.log('Starting chimp in watch mode');
+
+	var chimpProcess = exec(chimpCommand + chimpSwitches);
+	chimpProcess.stdout.pipe(process.stdout);
+	chimpProcess.stderr.pipe(process.stderr);
+	chimpProcess.on('close', function(code) {
+		if (code > 0) {
+			console.log(opts.name, 'exited with code ' + code);
+			process.exit(code);
+		} else {
+			callback();
+		}
+	});
+}
+
+startMeteor(startChimp(function() {
+	console.log('Completed.');
+}));
