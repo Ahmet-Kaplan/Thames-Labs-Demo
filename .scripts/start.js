7a32bbfcc28f2be26c6e1ee178333c1a837ec58d
Maxime Guitet <maxime.guitet@cambridgesoftware.co.uk>
2016-03-30 17:42:49 +0100

    Switched from exec to spawn process

--- .scripts/start.js
+++ .scripts/start.js
@@ -1,32 +1,31 @@
 #!/usr/bin/env node
 
-const exec = require('child_process').exec,
-	path = require('path'),
-	extend = require('util')._extend;
+const spawn = require('child_process').spawn,
+			path = require('path'),
+			extend = require('util')._extend;
 
-const meteorCommand = 'meteor --settings settings.json',
-	meteorProcessOptions = {
-		cwd: path.resolve(__dirname, '..'),
-		env: extend(process.env, {
-			STRIPE_PK: "pk_test_W7Cx4LDFStOIaJ2g5DufAIaE",
-			STRIPE_SK: "sk_test_CEgjj8xNKSrQMUrqC4puiHxA",
-			CI: 1
-		})
-	};
+const meteorCommand = 'meteor',
+			meteorArgs = ['--settings', 'settings.json'],
+			meteorProcessOptions = {
+				cwd: path.resolve(__dirname, '..'),
+				env: extend(process.env, {
+					STRIPE_PK: "pk_test_W7Cx4LDFStOIaJ2g5DufAIaE",
+					STRIPE_SK: "sk_test_CEgjj8xNKSrQMUrqC4puiHxA",
+					CI: 1
+				}),
+			};
 
 const chimpCommand = 'chimp';
-var chimpSwitches = ' --ddp=http://localhost:3000 --path=tests/cucumber';
+var chimpArgs = ['--ddp=http://localhost:3000',  '--path=tests/cucumber'];
 
 if (!process.env.CI) {
-	chimpSwitches += ' --watch';
+	chimpArgs.push('--watch');
 }
 
 function startMeteor() {
 	console.log('Starting Meteor');
 
-	var meteorProcess = exec(meteorCommand, meteorProcessOptions);
-	meteorProcess.stdout.pipe(process.stdout);
-	meteorProcess.stderr.pipe(process.stderr);
+	var meteorProcess = spawn(meteorCommand, meteorArgs, meteorProcessOptions);
 
 	meteorProcess.on('exit', function(code, signal) {
 		console.log('Meteor exited via exit event with code ' + (code ? code : signal));
@@ -48,12 +47,15 @@ function startMeteor() {
 	});
 
 	meteorProcess.stdout.on('data', (data) => {
+		//data is a Buffer and needs to be converted to a string to be displayed
+		data = data.toString();
+		console.log(data);
 		if (data.match('App running at')) {
 			console.log('Starting Chimp');
 
-			var chimpProcess = exec(chimpCommand + chimpSwitches);
-			chimpProcess.stdout.pipe(process.stdout);
-			chimpProcess.stderr.pipe(process.stderr);
+			var chimpProcess = spawn(chimpCommand, chimpArgs, {
+				stdio: 'inherit'
+			});
 
 			chimpProcess.on('close', function(code, signal) {
 				meteorProcess.kill();
