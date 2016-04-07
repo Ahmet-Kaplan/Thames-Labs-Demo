436b2254dcb6db21f054980a05adb17c9f79bc6d
Damien <damien@cambridgesoftware.co.uk>
2016-04-07 10:00:50 +0100

    Corrected tip text regarding FAB

--- common/tips.js
+++ common/tips.js
@@ -3,27 +3,29 @@ TipList = [{
 }, {
 	"Tip": "Not sure what a button does? Hover over it for a tooltip!"
 }, {
+	"Tip": "You can toggle the fast-action button on mobile view by opening the main menu, clicking on the <i class='fa fa-fw fa-user'></i> icon and then tapping the Show/Hide Action Button option."
+}, {
 	"Tip": "You can re-take a tutorial as many times as you want to. Just click the <i class='fa fa-fw fa-question'></i> icon in the navigation bar at the top of the screen."
 }, {
-	"Tip": "Create a company from anywhere by pressing CTRL + ALT + C, or via the fast-action button (the <i class='fa fa-fw fa-plus'></i> icon in the lower-right corner)"
+	"Tip": "Create a company from anywhere by pressing CTRL + ALT + C, or via the fast-action button when on mobile (the <i class='fa fa-fw fa-plus'></i> icon in the lower-right corner)"
 }, {
 	"Tip": "Need to quickly search for something? Pressing CTRL + SHIFT + F will open the Global Search (as will the <i class='fa fa-fw fa-search'></i> icon in the navigation bar)."
 }, {
-	"Tip": "Create a contact from anywhere by pressing CTRL + ALT + X, or via the fast-action button (the <i class='fa fa-fw fa-plus'></i> icon in the lower-right corner)"
+	"Tip": "Create a contact from anywhere by pressing CTRL + ALT + X, or via the fast-action button when on mobile (the <i class='fa fa-fw fa-plus'></i> icon in the lower-right corner)"
 }, {
 	"Tip": "Need a better view of your upcoming tasks? Switch to Calendar View by clicking <i class='fa fa-fw fa-calendar'></i> on the Tasks list view."
 }, {
-	"Tip": "Create a project from anywhere by pressing CTRL + ALT + J, or via the fast-action button (the <i class='fa fa-fw fa-plus'></i> icon in the lower-right corner)"
+	"Tip": "Create a project from anywhere by pressing CTRL + ALT + J, or via the fast-action button when on mobile (the <i class='fa fa-fw fa-plus'></i> icon in the lower-right corner)"
 }, {
 	"Tip": "You can re-arrange your dashboard widgets by left-clicking and dragging the header bar of the widget you wish to move."
 }, {
-	"Tip": "Create a purchase order from anywhere by pressing CTRL + ALT + P, or via the fast-action button (the <i class='fa fa-fw fa-plus'></i> icon in the lower-right corner)"
+	"Tip": "Create a purchase order from anywhere by pressing CTRL + ALT + P, or via the fast-action button when on mobile (the <i class='fa fa-fw fa-plus'></i> icon in the lower-right corner)"
 }, {
 	"Tip": "Rename your favourites by clicking the <i class='fa fa-fw fa-wrench'></i> icon next to their entries in the Favourites list!"
 }, {
-	"Tip": "Create a product from anywhere by pressing CTRL + ALT + R, or via the fast-action button (the <i class='fa fa-fw fa-plus'></i> icon in the lower-right corner)"
+	"Tip": "Create a product from anywhere by pressing CTRL + ALT + R, or via the fast-action button when on mobile (the <i class='fa fa-fw fa-plus'></i> icon in the lower-right corner)"
 }, {
 	"Tip": "You can share your searches and filtered conditions with other users! When you search, the URL of the current page will update to include your conditions - simply copy it and send it in any way you choose."
 }, {
-	"Tip": "Create an opportunity from anywhere by pressing CTRL + ALT + O, or via the fast-action button (the <i class='fa fa-fw fa-plus'></i> icon in the lower-right corner)"
+	"Tip": "Create an opportunity from anywhere by pressing CTRL + ALT + O, or via the fast-action button when on mobile (the <i class='fa fa-fw fa-plus'></i> icon in the lower-right corner)"
 }]
