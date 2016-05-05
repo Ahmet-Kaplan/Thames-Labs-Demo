9458cfb52a79a764d12691f7be66ae42e5fca251
Max <maxime.guitet@cambridgesoftware.co.uk>
2016-05-05 15:44:04 +0100

    Fixed linting

--- server/methods/tenant.js
+++ server/methods/tenant.js
@@ -4,41 +4,41 @@ Meteor.methods({
       throw new Meteor.Error(403, 'Only superadmins may completely delete a tenant');
     }
 
-		if (!tenantId) return 'Tenant ID not supplied';
-		var tenant = Tenants.findOne({
-			_id: tenantId
-		});
-		if (!tenant) return 'Tenant not found';
-		try {
-			Partitioner.bindGroup(tenantId, function() {
-				console.log('Deleting tasks...');
-				Tasks.remove({});
-				console.log('Deleting tags...');
-				Meteor.tags.remove({});
-				console.log('Deleting events...');
-				//EventLog is not partitioned
-				EventLog.remove({
-					group: tenantId
-				});
-				console.log('Deleting companies...');
-				Companies.remove({});
-				console.log('Deleting contacts...');
-				Contacts.remove({});
-				console.log('Deleting opportunities...');
-				Opportunities.remove({});
-				console.log('Deleting projects...');
-				Projects.remove({});
-				console.log('Deleting purchase order items...');
-				PurchaseOrderItems.remove({});
-				console.log('Deleting purchase orders...');
-				PurchaseOrders.remove({});
-				console.log('Deleting chatter...');
-				Chatterbox.remove({});
-				console.log('Deleting products...');
-				Products.remove({});
-				console.log('Deleting activities...');
-				Activities.remove({});
-			});
+    if (!tenantId) return 'Tenant ID not supplied';
+    var tenant = Tenants.findOne({
+      _id: tenantId
+    });
+    if (!tenant) return 'Tenant not found';
+    try {
+      Partitioner.bindGroup(tenantId, function() {
+        console.log('Deleting tasks...');
+        Tasks.remove({});
+        console.log('Deleting tags...');
+        Meteor.tags.remove({});
+        console.log('Deleting events...');
+        //EventLog is not partitioned
+        EventLog.remove({
+          group: tenantId
+        });
+        console.log('Deleting companies...');
+        Companies.remove({});
+        console.log('Deleting contacts...');
+        Contacts.remove({});
+        console.log('Deleting opportunities...');
+        Opportunities.remove({});
+        console.log('Deleting projects...');
+        Projects.remove({});
+        console.log('Deleting purchase order items...');
+        PurchaseOrderItems.remove({});
+        console.log('Deleting purchase orders...');
+        PurchaseOrders.remove({});
+        console.log('Deleting chatter...');
+        Chatterbox.remove({});
+        console.log('Deleting products...');
+        Products.remove({});
+        console.log('Deleting activities...');
+        Activities.remove({});
+      });
 
       console.log('Deleting users...');
       Meteor.users.remove({
