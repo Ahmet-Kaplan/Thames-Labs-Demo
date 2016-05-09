8c0f71ea3b9d70c842a811da34be8435aabb0f6a
Ben <ben.miles@cambridgesoftware.co.uk>
2016-05-09 09:22:38 +0100

    Fix error

--- tests/cucumber/features/_old_steps/search_steps.js
+++ tests/cucumber/features/_old_steps/search_steps.js
@@ -1,7 +1,7 @@
 module.exports = function() {
   this.When(/^I set the filter to "([^"]*)" then "([^"]*)"$/, function(filter, value) {
-    var selectizeInput = 'input#filterBox + .selectize-control>.selectize-input>input',
-        selectizeDropdown = 'input#filterBox + .selectize-control>.selectize-dropdown';
+    var selectizeInput = 'input#filterBox + .selectize-control>.selectize-input>input';
+        // selectizeDropdown = 'input#filterBox + .selectize-control>.selectize-dropdown';
     browser.waitForExist(selectizeInput, 5000);
     browser.waitForVisible(selectizeInput, 5000);
     browser.setValue(selectizeInput, filter);
