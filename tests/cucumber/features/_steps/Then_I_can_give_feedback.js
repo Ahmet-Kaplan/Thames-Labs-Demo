15975bad660bef7bbef2775e1c1110d7e3e26d18
Ben <ben.miles@cambridgesoftware.co.uk>
2016-05-19 10:01:06 +0100

    Fixed the feeback test (For real this time)

--- tests/cucumber/features/_steps/Then_I_can_give_feedback.js
+++ tests/cucumber/features/_steps/Then_I_can_give_feedback.js
@@ -1,7 +1,10 @@
 module.exports = function() {
   this.Then(/^I can give feedback$/, function() {
-    browser.click('#feedback-link');
+    browser.safeClick('#feedback-link');
     browser.waitForModal();
     expect(browser.isExisting('h4=Feedback')).toBe(true);
+    browser.execute(function() {
+      Modal.hide();
+    });
   });
 };
