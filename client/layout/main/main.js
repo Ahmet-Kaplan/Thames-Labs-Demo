4c29d8fc54c01ab1aa23d0cf2fcbf395a757c92e
Jamie Collinson <jamiecollinson@gmail.com>
2016-05-20 16:56:20 +0100

    Remove tawk mk2

--- client/layout/main/main.js
+++ client/layout/main/main.js
@@ -29,17 +29,3 @@ Template.appLayout.onRendered(function() {
     threshold: 75,
   });
 });
-
-Template.onCreated(function() {
-  // this.autorun(function() {
-  //   if (Meteor.user() && !Roles.userIsInRole(Meteor.userId(), ['superadmin']) && Meteor.isProduction) {
-  //     $.getScript('https://embed.tawk.to/56b333a5fe87529955d980fa/default');
-      //.then(function(){
-      //   Tawk_API.visitor = {
-      //     name: Meteor.user().profile.name,
-      //     email: Meteor.user().emails[0].address
-      //   }
-      // });
-    }
-  });
-});
