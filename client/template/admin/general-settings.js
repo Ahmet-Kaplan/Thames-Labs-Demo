cce059f5b655a18e6fc39fdadbf0814a7118c717
Damien <damien@cambridgesoftware.co.uk>
2016-06-13 10:18:29 +0100

    Added ability to change tenant name from tenant admin screen

--- client/template/admin/general-settings.js
+++ client/template/admin/general-settings.js
@@ -8,5 +8,30 @@ Template.generalSettings.events({
         toastr.success('Your currency has been updated to <b>' + res.toUpperCase() + '</b>');
       }
     });
+  },
+  'click #saveTenantName': function(event, template) {
+    event.preventDefault();
+    var newName = $('#tenantNameEditor').val();
+    if (newName === "") {
+      toastr.error('You must provide a tenant name.');
+      return;
+    }
+    var user = Meteor.user();
+    var tenant = Tenants.findOne({_id: user.group});
+
+    Tenants.update(tenant._id, {$set: {"name": newName}}, function(err) {
+      if(err) {
+        toastr.error(err.reason);
+        $('#tenantNameEditor').val(tenant.name);
+      }
+    });
+
+    toastr.success("Tenant name updated successfully.");
   }
+});
+
+Template.generalSettings.onRendered(function() {
+  var user = Meteor.user();
+  var tenant = Tenants.findOne({_id: user.group});
+  $('#tenantNameEditor').val(tenant.name);
 });
