1339dafb962fe26cb1e6af9edb6d24f12b0f2232
Damien <damien@cambridgesoftware.co.uk>
2016-06-14 09:48:41 +0100

    Add author and ID fields to opps exporter.

--- client/template/opportunities/opportunities-detail.js
+++ client/template/opportunities/opportunities-detail.js
@@ -401,6 +401,7 @@ Template.opportunityDetail.events({
     reader.readAsArrayBuffer(file);
   },
   'change #template-upload': function(event) {
+
     var file = event.target.files[0];
     if (!file) return;
 
@@ -411,6 +412,7 @@ Template.opportunityDetail.events({
       var companyName = "";
       var companyAddress = "";
       var contactName = "";
+      var userName = Meteor.user().profile.name;
 
       if (this.companyId) {
         var company = Companies.findOne(this.companyId);
@@ -433,7 +435,9 @@ Template.opportunityDetail.events({
         var obj = {
           name: oi.name,
           description: oi.description,
-          value: oi.value
+          value: oi.value,
+          quantity: oi.quantity,
+          total: Number(oi.value * oi.quantity).toFixed(2)
         };
         items.push(obj);
       });
@@ -445,7 +449,9 @@ Template.opportunityDetail.events({
         "date": date,
         "lineItems": items,
         "opportunityName": opp.name,
-        "opportunityDescription": opp.description
+        "opportunityDescription": opp.description,
+        "author": userName,
+        "opportunityNumber": opp.sequencedIdentifier
       });
 
       doc.render();
