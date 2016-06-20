ba51ef1c4a85b8cb469c0f462e7f4d26e8a82f04
Damien <damien@cambridgesoftware.co.uk>
2016-06-20 11:16:09 +0100

    Refactor code

--- server/security.js
+++ server/security.js
@@ -96,15 +96,14 @@ Opportunities.allowTags(function(userId) {
 });
 
 ServerSession.setCondition(function(key, value) {
+  // Check for user ID. If not there, forcibly return true to ensure that the server can do whatever it needs to.
+  // If this is a user request, check which key they're trying to set and whether or not they're a super
+  // before allowing the change to take place
   if(Meteor.userId()) {
-    if(key !== "DocxToPdfRemaining") {
-      if(Roles.userIsInRole(Meteor.userId(), 'superadmin')) {
-        return true;
-      }
-      return false;
-
+    if(key !== "DocxToPdfRemaining" && Roles.userIsInRole(Meteor.userId(), 'superadmin')) {
+      return true;
     }
-    return true;
+    return false;
   }
   return true;
 });
