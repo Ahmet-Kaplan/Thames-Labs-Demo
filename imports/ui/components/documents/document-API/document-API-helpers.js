d89a547ae49f4dc25b7fa923a6cd29503aff5e3b
Jason Mashinchi <jason@mashinchi.com>
2016-06-29 12:45:04 +0100

    Refactored document modules
    - Fixes a breaking change in Meteor 1.3.4 latest version of meteor: not
    upgraded to it yet.

--- imports/ui/components/documents/document-API/document-API-helpers.js
+++ imports/ui/components/documents/document-API/document-API-helpers.js
@@ -1,75 +1,82 @@
-documentAPI = {};
+import { boxChooser } from './document-API-box.js';
+import { dropboxChooser } from './document-API-dropbox.js';
+import { googleChooser } from './document-API-google.js';
+import { onedriveChooser } from './document-API-onedrive.js';
 
-documentAPI.loadScripts = function() {
-  if (typeof Dropbox === 'undefined') {
-    $.getScript('https://www.dropbox.com/static/api/2/dropins.js').then(() => {
-      Dropbox.appKey = Meteor.settings.public.dropboxApiKey;
+const documentAPI = {
+  boxChooser: boxChooser,
+  dropboxChooser: dropboxChooser,
+  googleChooser: googleChooser,
+  onedriveChooser: onedriveChooser,
+  loadScripts: function() {
+    if (typeof Dropbox === 'undefined') {
+      $.getScript('https://www.dropbox.com/static/api/2/dropins.js').then(() => {
+        Dropbox.appKey = Meteor.settings.public.dropboxApiKey;
+      });
+    }
+    if (typeof BoxSelect === 'undefined') {
+      $.getScript('https://app.box.com/js/static/select.js');
+    }
+    if (typeof gapi === 'undefined') {
+      $.getScript('https://apis.google.com/js/api.js').then(() => {
+        gapi.load('auth');
+        gapi.load('picker');
+      });
+    }
+    if (typeof OneDrive === 'undefined') {
+      $.getScript('https://js.live.net/v7.0/OneDrive.js');
+    }
+  },
+  addDocument: function(collectionName, id, documentData) {
+    var mongoDoc = null;
+    var docAlreadyExists = false;
+    var docWasUpdated = false;
+
+    mongoDoc = Collections[collectionName].findOne(id);
+    var currDocs = [];
+    _.each(mongoDoc.documents, function(doc) {
+      if ((doc.docName === documentData.docName) && (doc.docPath === documentData.docPath)) {
+        docAlreadyExists = true;
+      } else if (doc.docPath === documentData.docPath) {
+        docWasUpdated = true;
+      } else {
+        currDocs.push(doc);
+      }
     });
-  }
-  if (typeof BoxSelect === 'undefined') {
-    $.getScript('https://app.box.com/js/static/select.js');
-  }
-  if (typeof gapi === 'undefined') {
-    $.getScript('https://apis.google.com/js/api.js').then(() => {
-      gapi.load('auth');
-      gapi.load('picker');
-    });
-  }
-  if (typeof OneDrive === 'undefined') {
-    $.getScript('https://js.live.net/v7.0/OneDrive.js');
-  }
-};
 
-documentAPI.addDocument = function(collectionName, id, documentData) {
-  var mongoDoc = null;
-  var docAlreadyExists = false;
-  var docWasUpdated = false;
+    if (!docAlreadyExists) {
+      currDocs.push(documentData);
+      Collections[collectionName].update(id, {
+        $set: {
+          documents: currDocs
+        }
+      });
+    }
 
-  mongoDoc = Collections[collectionName].findOne(id);
-  var currDocs = [];
-  _.each(mongoDoc.documents, function(doc) {
-    if ((doc.docName === documentData.docName) && (doc.docPath === documentData.docPath)) {
-      docAlreadyExists = true;
-    } else if (doc.docPath === documentData.docPath) {
-      docWasUpdated = true;
+    if (docAlreadyExists) {
+      toastr.clear();
+      toastr.warning('The selected document has already been added');
+    } else if (docWasUpdated) {
+      toastr.clear();
+      toastr.info('The selected document\'s URL was already referenced, but under a different name. We\'ve updated the list to show the new name.');
     } else {
-      currDocs.push(doc);
+      toastr.clear();
+      toastr.success('The selected document has been added');
     }
-  });
-
-  if (!docAlreadyExists) {
-    currDocs.push(documentData);
+  },
+  removeDocument: function(collectionName, id, document) {
     Collections[collectionName].update(id, {
-      $set: {
-        documents: currDocs
+      $pull: {
+        documents: {
+          docPath: document.docPath,
+          docName: document.docName
+        }
       }
     });
+
+    toastr.clear();
+    toastr.success('The selected document has been successfully removed');
   }
-
-  if (docAlreadyExists) {
-    toastr.clear();
-    toastr.warning('The selected document has already been added');
-  } else if (docWasUpdated) {
-    toastr.clear();
-    toastr.info('The selected document\'s URL was already referenced, but under a different name. We\'ve updated the list to show the new name.');
-  } else {
-    toastr.clear();
-    toastr.success('The selected document has been added');
-  }
-};
-
-documentAPI.removeDocument = function(collectionName, id, document) {
-  Collections[collectionName].update(id, {
-    $pull: {
-      documents: {
-        docPath: document.docPath,
-        docName: document.docName
-      }
-    }
-  });
-
-  toastr.clear();
-  toastr.success('The selected document has been successfully removed');
 };
 
 export { documentAPI };
