71cfff27f008c5627f8192cf9c9d11bbf656b1d0
Damien <damien@cambridgesoftware.co.uk>
2016-07-01 09:04:20 +0100

    Adds an event to the render function of the placeholder page so we know when people encounter it

--- client/template/placeholder/placeholder.js
+++ client/template/placeholder/placeholder.js
@@ -1,3 +1,9 @@
+Template.placeholder.onRendered(function() {
+  if(Meteor.user()) {
+    LogServerEvent(LogLevel.Warning, Meteor.user().profile.name + " landed on the placeholder page when trying to access " + FlowRouter.current().path, null, null);
+  }
+});
+
 Template.placeholder.events({
   'click #feedback-link': function(event) {
     event.preventDefault();
