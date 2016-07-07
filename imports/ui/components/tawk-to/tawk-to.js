bbc20a732c66810adbfa7fef07709b975d472a2b
Max <maxime.guitet@cambridgesoftware.co.uk>
2016-07-07 10:48:20 +0100

    Latest fix

--- imports/ui/components/tawk-to/tawk-to.js
+++ imports/ui/components/tawk-to/tawk-to.js
@@ -2,30 +2,22 @@ import { Meteor } from 'meteor/meteor';
 
 function loadTawkTo() {
   if(Meteor.isProduction && !Meteor.isTest) {
-    if(Meteor.user() && !Roles.userIsInRole(Meteor.userId(), ['superadmin'])) {
-      /* eslint-disable camelcase*/
-      Tawk_API = {
-        visitor: Meteor.user().profile.name,
-        email: Meteor.user().emails[0].address
-      };
-      /* eslint-enable camelcase*/
-    }
-
-    const isCSUser = (Meteor.user() && _.get(Meteor.user(), 'emails[0].address') && Meteor.user().emails[0].address.indexOf('@cambridgesoftware.co.uk') === -1);
-    if(!isCSUser){
-      $.getScript('https://embed.tawk.to/56b333a5fe87529955d980fa/default');
-    }
+    $.getScript('https://embed.tawk.to/56b333a5fe87529955d980fa/default');
   }
 }
 
 function updateTawkToVisitor(visitor, tenant) {
   /* eslint-disable camelcase*/
   if(typeof visitor !== 'undefined' && !!tenant) {
-    Meteor.call('tawkTo.UserInfo', function(err, res) {
-      if(!!res && typeof Tawk_API !== 'undefined' && _.get(Tawk_API, 'setAttributes')) {
-        Tawk_API.setAttributes(res);
-      }
-    });
+    if(visitor.emails[0].address.indexOf('@cambridgesoftware.co.uk') === -1) {
+      Meteor.call('tawkTo.UserInfo', function(err, res) {
+        if(!!res && typeof Tawk_API !== 'undefined' && _.get(Tawk_API, 'setAttributes')) {
+          Tawk_API.setAttributes(res);
+        }
+      });
+    } else {
+      $('#tawkchat-iframe-container').remove();
+    }
   }
   /* eslint-enable camelcase*/
 }
