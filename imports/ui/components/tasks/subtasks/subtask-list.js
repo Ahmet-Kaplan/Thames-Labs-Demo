195636da0533a746380b1101577583024e970dbd
Jason Mashinchi <jason@mashinchi.com>
2016-07-18 12:31:41 +0100

    Split out subtask item template

--- imports/ui/components/tasks/subtasks/subtask-list.js
+++ imports/ui/components/tasks/subtasks/subtask-list.js
@@ -1,3 +1,4 @@
+import './subtask-item.js';
 import './subtask-list.html';
 
 Template.subtaskList.events({
@@ -15,32 +16,5 @@ Template.subtaskList.helpers({
   subtasks: function() {
     var subs = ReactiveMethod.apply("tasks.getSubTasks", [Template.currentData()._id]);
     if (subs && subs.length > 0) return subs;
-  }
-});
-
-Template.subtaskItem.onCreated(function() {
-  this.subscribe('taskById', this.data._id);
-  this.state = new ReactiveVar(this.data.completed);
-});
-
-Template.subtaskItem.onRendered(function() {
-  var myId = this.data._id;
-
-  this.autorun(function() {
-    var subtask = Tasks.findOne({
-      _id: myId
-    });
-    if (subtask) {
-      Template.instance().state.set(subtask.completed);
-    }
-  });
-});
-
-Template.subtaskItem.helpers({
-  subtaskCompleted: function() {
-    return Template.instance().state.get();
-  },
-  subtaskName: function() {
-    return this.title;
   }
 });
