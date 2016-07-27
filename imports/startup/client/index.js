29dbb355216e0962b8276c17a194e5f4ab224dc9
Damien <damien@cambridgesoftware.co.uk>
2016-07-27 12:34:04 +0100

    Breadcrumbs prototype

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -4,3 +4,4 @@ import '../../ui/components/documents/document-container.js';
 import '../../ui/layouts/footer/footer.js';
 import '../../ui/layouts/nav/nav.js';
 import '../../ui/pages/settings/settings.js';
+import '/imports/ui/components/breadcrumbs/breadcrumbs.js';
