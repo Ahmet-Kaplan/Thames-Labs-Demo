861dfc93a5029dc684186b5f18cf692d87d15add
Max <maxime.guitet@cambridgesoftware.co.uk>
2016-07-28 14:38:49 +0100

    Add ability to insert document from url

---
+++ imports/ui/components/documents/modals/url-document-modal.js
@@ -0,0 +1,26 @@
+import { documentAPI } from '../document-API/document-API-helpers.js';
+import './url-document-modal.html';
+
+Template.urlDocumentModal.events({
+  'click #addDocument': function(evt) {
+    evt.preventDefault();
+    const docName = $('#docName').val();
+    const docUrl = $('#docUrl').val();
+    if(!docName || !docUrl) {
+      toastr.warning('Please indicate both name and URL for the document.');
+      return;
+    } else if(!docUrl.match(/^https?:\/\/.+/)) {
+      toastr.warning('Invalid URL');
+      return;
+    }
+    const file = {
+      "docName": docName,
+      "docPath": docUrl,
+      "fileIcon": "link",
+      "service": "custom",
+      "serviceIcon": "link",
+    };
+    documentAPI.addDocument(this.collectionName, this.entityId, file);
+    Modal.hide();
+  }
+});
