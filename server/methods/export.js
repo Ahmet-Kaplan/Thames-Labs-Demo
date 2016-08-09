3f7b4033f138210a82f21db3946c9cd1267492ba
Jason Mashinchi <jason@mashinchi.com>
2016-08-09 10:50:19 +0100

    Removed unnecessary variable

--- server/methods/export.js
+++ server/methods/export.js
@@ -1,34 +1,3 @@
-// These column names are removed from all final exports
-// They may be used for interim processing - e.g. looking up related data
-const OMITTEDCOLUMNS = [
-  '_id',
-  'createdBy',
-  'companyId',
-  'contactId',
-  'opportunityId',
-  'projectId',
-  'purchaseOrderId',
-  'taskId',
-  'productId',
-  'currentStageId',
-  'items',
-  'userId',
-  'supplierCompanyId',
-  'supplierContactId',
-  'primaryEntityId',
-  'documents',
-  'customFields',
-  'extendedInformation',
-  'metadata',
-  'sequencedIdentifier',
-  'assigneeId',
-  'entityId',
-  'entityType',
-  'projectTypeId',
-  'projectMilestoneId',
-  'stripe'
-];
-
 Meteor.methods({
 
   'search.dataDump': function() {
