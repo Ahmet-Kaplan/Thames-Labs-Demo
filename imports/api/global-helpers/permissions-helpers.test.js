54d9dd11ca82cdf40d0dca44f9707d35fa9699e6
Jason Mashinchi <jason@mashinchi.com>
2016-08-12 12:58:31 +0100

    Added initial version of permissions-helpers tests

---
+++ imports/api/global-helpers/permissions-helpers.test.js
@@ -0,0 +1,45 @@
+import { chai } from 'meteor/practicalmeteor:chai';
+import { redirectWithoutPermission, superAdminOnly } from './permissions-helpers.js';
+
+describe("permission helpers", () => {
+  beforeEach(function() {
+    Roles.userIsInRole = (userId, permissions) => {
+      if (userId == "normaluser") {
+        if (permissions == "normalpermission") return true;
+      } else if (userId == "superuser") {
+        if (permissions == "superadmin") return true;
+      }
+      return false;
+    };
+  });
+
+  it("redirects user without permission", function(done) {
+    FlowRouter.go = (route) => {
+      chai.assert.equal(route, 'dashboard');
+      done();
+    };
+    redirectWithoutPermission("normaluser", "invalidpermission");
+  });
+
+  it("permits user with permission", function() {
+  });
+
+  it("redirects superuser without permission", function(done) {
+    FlowRouter.go = (route) => {
+      chai.assert.equal(route, 'tenants');
+      done();
+    };
+    redirectWithoutPermission("superuser", "superadmin");
+  });
+
+  it("redirects normal user without superadmin permission", function(done) {
+    FlowRouter.go = (route) => {
+      chai.assert.equal(route, 'dashboard');
+      done();
+    };
+    superAdminOnly("normaluser");
+  });
+
+  it("permits superadmin with permission", function() {
+  });
+});
