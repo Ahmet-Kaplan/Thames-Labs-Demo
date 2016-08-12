fdf77c1e8fb548bdd413081b95503511d1b9f9ff
Jason Mashinchi <jason@mashinchi.com>
2016-08-12 16:21:35 +0100

    Moved currency helpers to API

---
+++ imports/api/currency/currency-helpers.js
@@ -0,0 +1,61 @@
+const currencyHelpers = {
+  //Return the currency symbol from the currency code e.g gbp => £
+  getCurrencySymbol(currency) {
+    const currencySymbol = {
+      gbp: '£',
+      eur: '€',
+      usd: '$'
+    };
+    return currencySymbol[currency] || '£';
+  },
+
+  //Return the current tenant's currency symbol. e.g £
+  userCurrencySymbol() {
+    if(!Meteor.user()) return '£';
+
+    const tenant = Tenants.findOne({
+      _id: Meteor.user().group
+    });
+    if (tenant) {
+      const currency = tenant.settings.currency || 'gbp';
+      return currencyHelpers.getCurrencySymbol(currency);
+    }
+  },
+
+  //Returns the current tenant's currency type. e.g gbp
+  userCurrency() {
+    if(!Meteor.user()) return 'gbp';
+
+    const tenant = Tenants.findOne({
+      _id: Meteor.user().group
+    });
+    if (tenant) return tenant.settings.currency || 'gbp';
+  },
+
+  //Returns a formatted price. Takes a number, returns price. e.g 42 => '£42.00'
+  toDecimal(number) {
+    if (!number) number = 0;
+    number = parseFloat(number);
+
+    if(!Meteor.user()) return number.toFixed(2);
+
+    const allowedCurrencies = ['gbp', 'eur', 'usd'];
+    const tenantCurrency = Tenants.findOne({
+      _id: Meteor.user().group
+    }).settings.currency;
+    const currency = (allowedCurrencies.indexOf(tenantCurrency) === -1) ? 'gbp' : tenantCurrency;
+    const currencyLocale = {
+      gbp: 'en-gb',
+      eur: 'fr',
+      usd: 'en'
+    };
+    const displayLocale = currencyLocale[currency] || 'en-gb';
+
+    return number.toLocaleString(displayLocale, {
+      style: 'currency',
+      currency: currency.toUpperCase()
+    });
+  }
+};
+
+export { currencyHelpers };
--- imports/ui/components/products/product-list-item.js
+++ imports/ui/components/products/product-list-item.js
@@ -1,3 +1,4 @@
+import { currencyHelpers } from '/imports/api/currency/currency-helpers.js';
 import './product-list-item.html';
 
 Template.productListItem.helpers({
@@ -5,5 +6,14 @@ Template.productListItem.helpers({
     const searchDef = Template.currentData().index.getComponentDict().get('searchDefinition');
     const pattern = new RegExp(searchDef, 'gi');
     return Template.currentData().name.replace(pattern, '<span class="highlighted-search">$&</span>');
+  },
+  decimalCost: function() {
+    return currencyHelpers.toDecimal(Template.currentData().cost);
+  },
+  decimalPrice: function() {
+    return currencyHelpers.toDecimal(Template.currentData().price);
+  },
+  userCurrencyIcon: function() {
+    return currencyHelpers.userCurrency();
   }
 });
