7c4e7e781fcefdea8dd80c93097fb83d974651b3
Jason Mashinchi <jason@mashinchi.com>
2016-08-15 10:56:12 +0100

    Removed unnecessary event handler

--- imports/ui/pages/products/product-detail.js
+++ imports/ui/pages/products/product-detail.js
@@ -54,9 +54,5 @@ Template.productDetail.events({
   'click #edit-product': function(event) {
     event.preventDefault();
     Modal.show('updateProductModal', this);
-  },
-  'click #fab': function(event) {
-    event.preventDefault();
-    Modal.show('updateProductModal', this);
   }
 });
