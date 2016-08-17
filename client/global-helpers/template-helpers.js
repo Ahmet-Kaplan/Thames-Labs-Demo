e4d4d529bbcdf5691175e8e41c58a82ab25e8cc4
Jason Mashinchi <jason@mashinchi.com>
2016-08-17 14:55:57 +0100

    Updated some packages, removed duplicate browser type package

--- client/global-helpers/template-helpers.js
+++ client/global-helpers/template-helpers.js
@@ -86,6 +86,10 @@ Template.registerHelper("isMobile", function() {
   return bowser.mobile || bowser.tablet;
 });
 
+Template.registerHelper("isDesktop", function() {
+  return !bowser.mobile && !bowser.tablet;
+});
+
 Template.registerHelper("isApp", function() {
   return Meteor.isCordova;
 });
