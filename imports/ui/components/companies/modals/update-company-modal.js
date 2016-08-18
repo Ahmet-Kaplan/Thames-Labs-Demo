e524512b1fa2b64a906440c7a077856b1696ca7d
Jason Mashinchi <jason@mashinchi.com>
2016-08-18 14:43:31 +0100

    Move company hooks

--- imports/ui/components/companies/modals/update-company-modal.js
+++ imports/ui/components/companies/modals/update-company-modal.js
@@ -26,3 +26,30 @@ Template.updateCompanyModal.events({
     Modal.hide();
   }
 });
+
+AutoForm.hooks({
+  updateCompanyForm: {
+    before: {
+      update: function(doc) {
+        var oldValues = this.currentDoc,
+            modifications = true;
+        $.each(['address', 'address2', 'city', 'country', 'county', 'postcode'], function(i, field) {
+          modifications = (oldValues[field] === doc.$set[field]);
+          return modifications;
+        });
+        if (!modifications) {
+          doc.$set.lat = '';
+          doc.$set.lng = '';
+        }
+        return doc;
+      }
+    },
+    onSuccess: function() {
+      Modal.hide();
+      toastr.success('Company details updated.');
+    },
+    onError: function(formType, error) {
+      toastr.error('Company update error: ' + error);
+    }
+  }
+});
