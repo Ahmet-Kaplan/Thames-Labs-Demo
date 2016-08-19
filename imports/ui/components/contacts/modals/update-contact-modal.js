2c8a34acd667dc579fe990a1143a9c7b90679537
Jason Mashinchi <jason@mashinchi.com>
2016-08-19 14:42:17 +0100

    Restored contact map changes

--- imports/ui/components/contacts/modals/update-contact-modal.js
+++ imports/ui/components/contacts/modals/update-contact-modal.js
@@ -9,10 +9,6 @@ Template.updateContactModal.onCreated(function() {
 });
 
 Template.updateContactModal.onRendered(function() {
-  if (typeof this.data.companyId === "undefined") {
-    $('#addressWrapper').show();
-  }
-
   if (Meteor.user()) {
     const tenant = Tenants.findOne({
       _id: Meteor.user().group
@@ -39,54 +35,6 @@ Template.updateContactModal.onRendered(function() {
       });
     }
   }
-
-  this.autorun(function() {
-    if (GoogleMaps.loaded()) {
-      $("#geo").geocomplete({
-        details: "#editContactForm",
-        detailsAttribute: "data-geo"
-      }).bind("geocode:result", function(event, result) {
-        let address = "",
-            strNumber = _.find(result.address_components, function(elt) {
-              return elt.types[0] == "street_number";
-            });
-
-        if (typeof strNumber !== 'undefined') {
-          strNumber = strNumber.long_name;
-          address += strNumber + " ";
-        }
-
-        let route = _.find(result.address_components, function(elt) {
-          return elt.types[0] == "route";
-        });
-
-        if (typeof route !== 'undefined') {
-          route = route.long_name;
-          address += route;
-        }
-        $("#formatted_address").val(address);
-        $("#mapModal_canvas").empty();
-        const map = new google.maps.Map(document.getElementById("mapModal_canvas"), {
-                zoom: 16,
-                center: result.geometry.location,
-                scrollwheel: false
-              }),
-              marker = new google.maps.Marker({
-                map: map,
-                position: result.geometry.location,
-                draggable: true
-              });
-        google.maps.event.addListener(marker, "dragend", function() {
-          $("input[name=lat]").val(marker.getPosition().lng());
-          $("input[name=lng]").val(marker.getPosition().lat());
-        });
-      }).keypress(function(event) {
-        if (event.keyCode == 13) {
-          $("#address_details").show();
-        }
-      });
-    }
-  });
 });
 
 Template.updateContactModal.helpers({
@@ -99,83 +47,20 @@ Template.updateContactModal.helpers({
     });
     if (tenant && tenant.settings.contact.titles && tenant.settings.contact.titles.length > 0) return true;
     return false;
-  }
-});
-
-Template.updateContactModal.events({
-  'click #show-map': function() {
-    $("#show-map").hide();
-    $("#map_wrapper").show();
-    $("#mapModal_canvas").height("400px");
-    let location = {
-      lat: 52.234744,
-      lng: 0.153752
+  },
+  addressData: function() {
+    const currentData = Template.currentData();
+    const address = {
+      address: currentData.address,
+      address2: currentData.address2,
+      city: currentData.city,
+      postcode: currentData.postcode,
+      county: currentData.county,
+      country: currentData.country,
+      lng: currentData.lng,
+      lat: currentData.lat
     };
-    const mapData = this,
-          mapModal = new google.maps.Map(document.getElementById("mapModal_canvas"), {
-            zoom: 10,
-            center: location,
-            scrollwheel: false
-          });
-
-    if (mapData !== null && typeof mapData.title !== "undefined") {
-      mapData.title = mapData.forename + ' ' + mapData.surname;
-    } else {
-      mapData.title = mapData.forename + ' ' + mapData.surname;
-    }
-    //Set map to the current location
-    if (typeof mapData.lat !== "undefined" && typeof mapData.lng !== "undefined") {
-      location = {
-        lat: parseFloat(mapData.lat),
-        lng: parseFloat(mapData.lng)
-      };
-      mapModal.panTo(location);
-      mapModal.setZoom(16);
-
-      const markerModal = new google.maps.Marker({
-        map: mapModal,
-        position: location,
-        title: mapData.title,
-        draggable: true
-      });
-      markerModal.setMap(mapModal);
-      google.maps.event.addListener(markerModal, "dragend", function(event) {
-        $("input[name=lat]").val(markerModal.getPosition().lat());
-        $("input[name=lng]").val(markerModal.getPosition().lng());
-      });
-      const infowindow = new google.maps.InfoWindow();
-      infowindow.setContent(mapData.title);
-      infowindow.open(mapModal, markerModal);
-    } else {
-      const gc = new google.maps.Geocoder();
-      gc.geocode({
-        'address': mapData.address + mapData.postcode + mapData.city + mapData.country
-      }, function(results, status) {
-        if (status == google.maps.GeocoderStatus.OK) {
-          location = {
-            lat: results[0].geometry.location.lat(),
-            lng: results[0].geometry.location.lng()
-          };
-          mapModal.panTo(location);
-          mapModal.setZoom(16);
-
-          const markerModal = new google.maps.Marker({
-            map: mapModal,
-            position: location,
-            title: mapData.title,
-            draggable: true
-          });
-          markerModal.setMap(mapModal);
-          google.maps.event.addListener(markerModal, "dragend", function(event) {
-            $("input[name=lat]").val(markerModal.getPosition().lat());
-            $("input[name=lng]").val(markerModal.getPosition().lng());
-          });
-          const infowindow = new google.maps.InfoWindow();
-          infowindow.setContent(mapData.title);
-          infowindow.open(mapModal, markerModal);
-        }
-      });
-    }
+    return address;
   }
 });
 
