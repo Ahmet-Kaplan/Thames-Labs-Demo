43b8a6d77aa16f005ff92e40e28492977950f250
Ben <ben.miles@cambridgesoftware.co.uk>
2016-08-19 13:09:10 +0100

    Merge entity count with filter display

--- imports/ui/components/search/filtering/panel/filter-panel.js
+++ imports/ui/components/search/filtering/panel/filter-panel.js
@@ -4,7 +4,25 @@ import '../box/filter-box.js';
 import '../tag/filter-tag.js';
 import { getFilters } from '/imports/api/search/search-functions.js';
 
+Template.filterPanel.onCreated(function() {
+  this.totalCount = new ReactiveVar(0);
+});
+
+Template.filterPanel.onRendered(function() {
+  const mainCollectionName = Template.instance().data.collectionName;
+  console.log(Template.instance());
+  this.autorun(() => {
+    this.totalCount.set(Collections[mainCollectionName].index.getComponentDict().get('count'));
+  });
+});
+
 Template.filterPanel.helpers({
+  entityCount: function() {
+    return Template.instance().totalCount.get();
+  },
+  multiple: function() {
+    return Template.instance().totalCount.get() !== 1;
+  },
   filtersList: function() {
     return getFilters(Template.instance().data.collectionName);
   },
