67a88adcb653057c7eb24ee976cbd10bfa4f3aec
Jason Mashinchi <jason@mashinchi.com>
2016-08-25 14:01:17 +0100

    Moved tags and currency methods to imports folder

--- imports/startup/server/register-api.js
+++ imports/startup/server/register-api.js
@@ -1,4 +1,6 @@
 import '/imports/api/billing/server/methods.js';
+import '/imports/api/currency/server/methods.js';
 import '/imports/api/export/server/methods.js';
 import '/imports/api/event-log/server/methods.js';
 import '/imports/api/notifications/server/methods.js';
+import '/imports/api/tags/server/methods.js';
