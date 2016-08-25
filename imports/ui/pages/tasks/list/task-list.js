ef7fa5b7ba26adb302767fb1572fb6335954b42a
Ben <ben.miles@cambridgesoftware.co.uk>
2016-08-25 16:09:50 +0100

    task list

--- imports/ui/pages/tasks/list/task-list.js
+++ imports/ui/pages/tasks/list/task-list.js
@@ -27,16 +27,6 @@ Template.taskList.onCreated(function() {
 
 Template.taskList.onRendered(function() {
 
-  // Watch for session variable setting search
-  Session.set('taskSearchQuery', null);
-  this.autorun(function() {
-    const searchQuery = Session.get('taskSearchQuery');
-    if (searchQuery) {
-      TasksIndex.getComponentMethods().search(searchQuery);
-      $('.stick-bar input').val(searchQuery);
-    }
-  });
-
   this.autorun(() => {
     this.totalTasks.set(Collections['tasks'].index.getComponentDict().get('count'));
   });
