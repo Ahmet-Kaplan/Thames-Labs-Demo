23380f70f9d7913387afcd341154fc369edccf8b
Jason Mashinchi <jason@mashinchi.com>
2016-09-01 13:12:11 +0100

    Added select picker code

--- imports/ui/components/settings/import/map-fields/map-fields.js
+++ imports/ui/components/settings/import/map-fields/map-fields.js
@@ -27,3 +27,40 @@ Template.mapFields.onRendered(function() {
     console.log(this.fieldMap.get());
   });
 });
+
+/*
+$('.selectpicker.map-field').selectpicker({
+        title: 'Do not import',
+        selectOnTab: true
+      });
+
+      //Setup event handlers, remove old ones
+      $('.field-selectpicker').off('changed.bs.select');
+      $('.field-selectpicker').on('changed.bs.select', (e) => {
+        //Update the mapping between importField (from csv) and schemaField
+        const currentFieldMap = instance.fieldMap.get();
+        const fieldArrayIndex = _.findIndex(currentFieldMap, ['schemaField', e.target.id.replace('-Selector', '')]);
+        currentFieldMap[fieldArrayIndex].importField = e.target.value;
+        instance.fieldMap.set(currentFieldMap);
+      });
+
+      $('.customfield-selectpicker').off('changed.bs.select');
+      $('.customfield-selectpicker').on('changed.bs.select', (e) => {
+        const globalCustomFields = instance.globalCustomFields.get();
+        const entityGCFs = instance.entityGCFs.get();
+        console.log(e);
+        const customFieldId = e.target.id.replace('-CustomFieldSelector', '');
+        globalCustomFields[fieldArrayIndex].importField = e.target.value;
+        instance.globalCustomFields.set(globalCustomFields);
+      });
+
+      //Preselect correct fields from CSV
+      const currentFieldMap = instance.fieldMap.get();
+      $('.field-selectpicker').each((i, obj) => {
+        const schemaFieldName = obj.id.replace('-Selector', '');
+        const field = _.find(currentFieldMap, { schemaField: schemaFieldName });
+        if (field) {
+          $(`#${obj.id}`).selectpicker('val', field.importField);
+        }
+      });
+      */
